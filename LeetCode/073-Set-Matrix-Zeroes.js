1a3436c55f43b6318e252965e67c0eef83699d98
Jesse Mavi <jesse.mavi@gmail.com>
2017-09-12 17:49:19 -0500

    Adds LeetCode 073-Set-Matrix-Zeroes

---
+++ LeetCode/073-Set-Matrix-Zeroes.js
@@ -0,0 +1,85 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    // Plan: iterate over the matrix. If a zero is found DFS in all four directions changing values to zero.
+
+    // Psuedocode:
+    // coordinatesToSet array
+
+    // setRowAndColumn function(matrix, row, column):
+        // iterate over matrix row
+            // set each value to 0
+        // iterate over matrix column
+            // set each value to 0
+
+    // iterate over matrix rows
+        // iterate over matrix columns
+            // if value is 0
+                // store row and column position in a tuple in coordinatesToSet array
+
+    // iterate over coordinatesToSet
+        // invoke setRowAndColumn with matrix, row, column
+
+    const coordinatesToSet = [];
+
+    const setRowAndColumn = (matrix, row, column) => {
+        for(let i = 0; i < matrix[row].length; i++) {
+            matrix[row][i] = 0;
+        }
+
+        for(let j = 0; j < matrix.length; j++) {
+            matrix[j][column] = 0;
+        }
+    }
+
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[0].length; j++) {
+            if(matrix[i][j] === 0) {
+                coordinatesToSet.push([i,j]);
+            }
+        }
+    }
+
+    for(let k = 0; k < coordinatesToSet.length; k++) {
+        setRowAndColumn(matrix, coordinatesToSet[k][0], coordinatesToSet[k][1]);
+    }
+
+    return matrix;
+};
+
+
+
+
+// example:
+/*
+[
+    [4,8,2,6],
+    [1,6,3,9],
+    [1,9,5,X],
+    [3,1,6,7],
+    [9,3,4,6],
+    [2,X,3,4]
+]
+[
+    [4,0,2,0],
+    [1,0,3,0],
+    [0,0,0,0],
+    [3,0,6,0],
+    [9,0,4,0],
+    [0,0,0,0]
+]
+*/
+
+// testing
+console.log(setZeroes(
+    [
+        [4,8,2,6],
+        [1,6,3,9],
+        [1,9,5,0],
+        [3,1,6,7],
+        [9,3,4,6],
+        [2,0,3,4]
+    ]
+));
