eea28a2e0764258fc41546d49736da67bc8de235
Jesse Mavi <jesse.mavi@gmail.com>
2018-10-16 18:37:56 -0500

    Updates LeetCode 155-Min-Stack

--- LeetCode/155-Min-Stack.js
+++ LeetCode/155-Min-Stack.js
@@ -2,9 +2,7 @@
  * initialize your data structure here.
  */
 var MinStack = function() {
-    this.storage_array = [];
-    // [3, 8, 2, 11, 1, 5]
-    // Plan: keep track of min for the stack at that point when a node is added
+    this.stack = [];
 };
 
 /** 
@@ -12,41 +10,32 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(x) {
-    let minVal;
-    let topMinVal;
-    
-    if(this.storage_array.length > 0) {
-        topMinVal = this.storage_array[this.storage_array.length - 1][1];
-    }
-    
-    if(topMinVal !== 'undefined' && topMinVal < x) {
-        minVal = topMinVal;
+    if(this.stack.length === 0 || x < this.stack[this.stack.length - 1][1]) {
+        this.stack.push([x,x]);
     } else {
-        minVal = x;
+        this.stack.push([x,this.stack[this.stack.length - 1][1]]);   
     }
-    
-    this.storage_array.push([x, minVal]);
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-    return this.storage_array.pop()[0];
+    return this.stack.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    return this.storage_array[this.storage_array.length - 1][0];
+    return this.stack[this.stack.length - 1][0];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return this.storage_array[this.storage_array.length - 1][1]
+    return this.stack[this.stack.length - 1][1];
 };
 
 /** 
@@ -57,3 +46,4 @@ MinStack.prototype.getMin = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+ 
